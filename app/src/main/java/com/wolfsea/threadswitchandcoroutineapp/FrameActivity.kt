afaf3f787420666d7877b82f09ef138b58aa9ba1
sharker <bigworld51931@126.com>
2021-08-09 23:39:15 +0800

    1.kotlin中的协程使用(二)

--- app/src/main/java/com/wolfsea/threadswitchandcoroutineapp/FrameActivity.kt
+++ app/src/main/java/com/wolfsea/threadswitchandcoroutineapp/FrameActivity.kt
@@ -41,17 +41,7 @@ class FrameActivity : AppCompatActivity() {
     private fun method1() {
         CoroutineScope(Dispatchers.Main).launch {
 
-            val dataSource = mutableListOf<String>()
-
-            dataSource.add("a11122")
-            dataSource.add("a11122-001")
-            dataSource.add("b11120-001")
-            dataSource.add("a11122-002")
-            dataSource.add("c21122")
-            dataSource.add("c21122-001")
-            dataSource.add("c21122-002")
-            dataSource.add("d31122-001")
-            dataSource.add("d31122-002")
+            val dataSource = createDataSet()
 
             val result = async(Dispatchers.IO) {
 
@@ -94,52 +84,57 @@ class FrameActivity : AppCompatActivity() {
     private fun method2() {
         CoroutineScope(Dispatchers.Main).launch {
 
-            val dataSource = mutableListOf<String>()
-
-            dataSource.add("a11122")
-            dataSource.add("a11122-001")
-            dataSource.add("b11120-001")
-            dataSource.add("a11122-002")
-            dataSource.add("c21122")
-            dataSource.add("c21122-001")
-            dataSource.add("c21122-002")
-            dataSource.add("d31122-001")
-            dataSource.add("d31122-002")
-
-            val result = withContext(Dispatchers.IO) {
-
-                val dataMap = mutableMapOf<String, MutableList<String>>()
-
-                for (value in dataSource) {
-
-                    val v1 = Util.cutStr(value)
-                    val v2 = Util.getSubStr(value)
-
-                    val keyExits = dataMap[v1] != null
-                    if (keyExits) {
-                        if (v2 != "-1") {
-
-                            val dataList = dataMap[v1]
-                            dataList?.add(v2)
-                        }
-                    } else {
-                        if (v2 != "-1") {
-
-                            val dataList = mutableListOf<String>()
-                            dataList.add(v2)
-                            dataMap[v1] = dataList
-                        }
-                    }
-                }
-
-                return@withContext dataMap
-            }
-
-            for ((key, value) in result) {
+            val dataSource = createDataSet()
+            val dataMap = getDataMap(dataSource)
+            for ((key, value) in dataMap) {
 
                 Log.d("value", "key:$key,value:${value.size}")
             }
         }
     }
 
+
+    private suspend fun createDataSet(): MutableList<String> = withContext(Dispatchers.IO) {
+        val dataSource = mutableListOf<String>()
+        dataSource.add("a11122")
+        dataSource.add("a11122-001")
+        dataSource.add("b11120-001")
+        dataSource.add("a11122-002")
+        dataSource.add("c21122")
+        dataSource.add("c21122-001")
+        dataSource.add("c21122-002")
+        dataSource.add("d31122-001")
+        dataSource.add("d31122-002")
+        dataSource
+    }
+
+    private suspend fun getDataMap(dataSource: MutableList<String>): MutableMap<String, MutableList<String>> =
+        withContext(Dispatchers.IO) {
+
+            val dataMap = mutableMapOf<String, MutableList<String>>()
+
+            for (value in dataSource) {
+
+                val v1 = Util.cutStr(value)
+                val v2 = Util.getSubStr(value)
+
+                val keyExits = dataMap[v1] != null
+                if (keyExits) {
+                    if (v2 != "-1") {
+
+                        val dataList = dataMap[v1]
+                        dataList?.add(v2)
+                    }
+                } else {
+                    if (v2 != "-1") {
+
+                        val dataList = mutableListOf<String>()
+                        dataList.add(v2)
+                        dataMap[v1] = dataList
+                    }
+                }
+            }
+
+            return@withContext dataMap
+        }
 }
